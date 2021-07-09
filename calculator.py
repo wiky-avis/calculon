a52c4e26a3dfbbb5f059cee7a17b79d0ebccfabd
wiky-avis <peta-holding@yandex.ru>
2021-07-09 15:08:38 +0300

    калькулятор

--- calculator.py
+++ calculator.py
@@ -6,12 +6,105 @@ from PyQt5.QtWidgets import *
 
 from mainwindow import Ui_MainWindow
 
+READY = 0
+INPUT = 1
 
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super(MyWindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+
+class MyWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MyWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+        for num in range(0, 10):
+            getattr(
+                self,
+                'pushButton_n%s' % num
+                ).pressed.connect(
+                    lambda v=num: self.input_number(v)
+                    )
+
+        self.pushButton_add.pressed.connect(
+            lambda: self.operation(operator.add))
+        self.pushButton_sub.pressed.connect(
+            lambda: self.operation(operator.sub))
+        self.pushButton_mul.pressed.connect(
+            lambda: self.operation(operator.mul))
+        self.pushButton_div.pressed.connect(
+            lambda: self.operation(operator.truediv))
+
+        self.pushButton_pc.pressed.connect(self.operation_pc)
+        self.pushButton_eq.pressed.connect(self.equals)
+
+        self.actionReset.triggered.connect(self.reset)
+        self.pushButton_ac.pressed.connect(self.reset)
+
+        self.actionExit.triggered.connect(self.close)
+
+        self.pushButton_m.pressed.connect(self.memory_store)
+        self.pushButton_mr.pressed.connect(self.memory_recall)
+
+        self.memory = 0
+        self.reset()
+
+        self.show()
+
+    def display(self):
+        self.lcdNumber.display(self.stack[-1])
+
+    def reset(self):
+        self.state = READY
+        self.stack = [0]
+        self.last_operation = None
+        self.current_op = None
+        self.display()
+
+    def memory_store(self):
+        self.memory = self.lcdNumber.value()
+
+    def memory_recall(self):
+        self.state = INPUT
+        self.stack[-1] = self.memory
+        self.display()
+
+    def input_number(self, v):
+        if self.state == READY:
+            self.state = INPUT
+            self.stack[-1] = v
+        else:
+            self.stack[-1] = self.stack[-1] * 10 + v
+
+        self.display()
+
+    def operation(self, op):
+        if self.current_op:
+            self.equals()
+
+        self.stack.append(0)
+        self.state = INPUT
+        self.current_op = op
+
+    def operation_pc(self):
+        self.state = INPUT
+        self.stack[-1] *= 0.01
+        self.display()
+
+    def equals(self):
+        if self.state == READY and self.last_operation:
+            s, self.current_op = self.last_operation
+            self.stack.append(s)
+
+        if self.current_op:
+            self.last_operation = self.stack[-1], self.current_op
+
+            try:
+                self.stack = [self.current_op(*self.stack)]
+            except Exception:
+                self.lcdNumber.display('Err')
+                self.stack = [0]
+            else:
+                self.current_op = None
+                self.state = READY
+                self.display()
 
 
 if __name__ == '__main__':
